59b9b83577bf81007bc17948e1aa898fd88483d6
marmag0 <mikolaj.mazur05@gmail.com>
2026-04-05 12:50:28 +0200

    database structure added

--- code/speedtest_app.py
+++ code/speedtest_app.py
@@ -1,27 +1,50 @@
 import speedtest
 from datetime import datetime, timezone
 
-def get_timestampz(now: datetime = datetime.now()):
+def get_timestampz():
     now_utc = datetime.now(timezone.utc)
     postgres_format = now_utc.isoformat(sep=' ')
     return postgres_format
 
+
 print(f"[*] {get_timestampz()}: Starting speedtest using Ofelia...")
 
+
+# speedtest module
 try:
-    st = speedtest.Speedtest()
+    start_time = get_timestampz()
+    st = speedtest.Speedtest(secure=True)
     st.get_best_server()
     st.download()
     st.upload()
 
+    finish_time = get_timestampz()
+    st_dict: dict = st.results.dict()
+
+
     results = st.results
 
-    download: float = results.download / 1_000_000  # Mbps
-    upload: float = results.upload / 1_000_000  # Mbps
+    download_speed: float = results.download / 1_000_000  # Mbps
+    upload_speed: float = results.upload / 1_000_000  # Mbps
     ping: float = results.ping  # ms
+except Exception as e:
+    print(f"[!] {get_timestampz()}: Speedtest error: {e}")
 
-    print(f"[*] Download: {download:.2f} Mbps; Upload: {upload:.2f} Mbps; Ping: {ping} ms")
+
+# saving to db module
+try:
+    pass
+
+except:
+    pass
+
+
+# debug log module
+try:
+    print(f"[*] Download: {download_speed:.2f} Mbps; Upload: {upload_speed:.2f} Mbps; Ping: {ping} ms")
+    print(f"[*] Speedtest server: {st_dict['server']}")
+    print(f"[*] Speedtest client: {st_dict['client']}")
     print(f"[+] {get_timestampz()}: Speedtest successful")
 
 except Exception as e:
-    print(f"[!] {get_timestampz()}: Speedtest error: {e}")
+    print(f"[!] {get_timestampz()}: Logging error: {e}")
